08c5bf5ac5fec94efdc86e45eaa1aa14f001642e
James <github@twelve.pw>
2020-06-03 12:59:39 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EngravingBoard from './Components/EngravingBoard'
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <EngravingBoard />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/EngravingBoard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EngravingBoard extends Component {
+  render () {
+    return (
+      <div>
+         Hi!
+      </div>
+    )
+  }
+}
