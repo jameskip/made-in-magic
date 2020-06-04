2c448cac949f4349c5f775edebd9565697aa2386
James <github@twelve.pw>
2020-06-03 23:51:28 -0600

    Updates

---
+++ precache-manifest.698576a68da1079902334e4d135216c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4104c134f3c0ed05e1f752acf8be0957",
+    "url": "/made-in-magic/index.html"
+  },
+  {
+    "revision": "cec4bc8256b7963c5156",
+    "url": "/made-in-magic/static/css/main.0e0ce8cd.chunk.css"
+  },
+  {
+    "revision": "8b8679f865c08da26359",
+    "url": "/made-in-magic/static/js/2.baf2e8ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/made-in-magic/static/js/2.baf2e8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec4bc8256b7963c5156",
+    "url": "/made-in-magic/static/js/main.73accbff.chunk.js"
+  },
+  {
+    "revision": "e6856623aec87ee64c3b",
+    "url": "/made-in-magic/static/js/runtime-main.21edd4bc.js"
+  },
+  {
+    "revision": "3d1e8c137058209800ad16cd8985659e",
+    "url": "/made-in-magic/static/media/MarkPro-Medium.3d1e8c13.woff"
+  },
+  {
+    "revision": "df57c68646186234d0b098ab76bb3fe0",
+    "url": "/made-in-magic/static/media/knife-preview.df57c686.png"
+  }
+]);
